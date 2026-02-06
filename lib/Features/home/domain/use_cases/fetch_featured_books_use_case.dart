e0dfb8cb5d7446f5c152258a532b78c55e3bc355
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-06 21:20:40 +0200

    fetch featured books use case done

---
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app_clean_architecture/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app_clean_architecture/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly_app_clean_architecture/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase({required this.homeRepo});
+   Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks(){
+    return homeRepo.fetchFeaturedBooks();
+   }
+}
+
+// use case تحقق priciple of singe responsibility
