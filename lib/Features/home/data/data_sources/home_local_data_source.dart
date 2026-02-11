e65a2118aa3fd8fa626ac419ee032af2fdcb8e4f
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-12 00:49:43 +0200

    add data local source done

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -1,3 +1,20 @@
-class HomeLocalDataSource { // from local data base
+import 'package:bookly_app_clean_architecture/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeLocalDataSource { // from local data base cashing  data from api
+ List<BookEntity> fetchFeaturedBooks();
+ List<BookEntity> fetcNewestBooks();
+}
+class HomeLocalDataSourceimpl extends HomeLocalDataSource{
+  @override
+  List<BookEntity> fetcNewestBooks() {
+    // TODO: implement fetcNewestBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
 
 }
