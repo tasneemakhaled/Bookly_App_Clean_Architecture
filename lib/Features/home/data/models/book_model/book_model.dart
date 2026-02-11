5b72d24988275452a4e13442a59733d4c5294bcb
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-11 21:51:05 +0200

    book model is a book entity

--- lib/Features/home/data/models/book_model/book_model.dart
+++ lib/Features/home/data/models/book_model/book_model.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app_clean_architecture/Features/home/domain/entities/book_entity.dart';
 import 'package:equatable/equatable.dart';
 
 import 'access_info.dart';
@@ -5,7 +6,7 @@ import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel extends Equatable {
+class BookModel extends  BookEntity {
   final String? kind;
   final String? id;
   final String? etag;
@@ -15,7 +16,7 @@ class BookModel extends Equatable {
   final AccessInfo? accessInfo;
   final SearchInfo? searchInfo;
 
-  const BookModel({
+   BookModel ({
     this.kind,
     this.id,
     this.etag,
@@ -24,7 +25,7 @@ class BookModel extends Equatable {
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(image: volumeInfo.imageLinks.thumbnail, title: volumeInfo.title!, authorName:volumeInfo.authors!.first, price: '', rating: volumeInfo.maturityRating, bookId: id);
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
     kind: json['kind'] as String?,
--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,11 +1,13 @@
-class BookEntity {  // data representation of ui not api
-final String image;
+class BookEntity { 
+   // data representation of ui not api
+final String? bookId;
+final String? image;
 final String title;
-final String authorName;
-final String price;
-final String rating;
+final String? authorName;
+final String? price;
+final String? rating;
 
-  BookEntity({required this.image, required this.title, required this.authorName, required this.price, required this.rating}); 
+  BookEntity({required this.image, required this.title, required this.authorName, required this.price, required this.rating, required this.bookId}); 
 
 }
 
