afe5390d53d16cacc3848ef61cdb31794e155e07
tasneemakhaled <khaledtasneem402@gmail.com>
2026-02-13 21:57:47 +0200

    create featured books cubit states

---
+++ lib/Features/home/presentation/manager/cubit/featured_books_state.dart
@@ -0,0 +1,21 @@
+part of 'featured_books_cubit.dart';
+
+sealed class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class FeaturedBooksInitial extends FeaturedBooksState {}
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+final class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity> books;
+
+ const FeaturedBooksSuccess({required this.books});
+}
+final class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+ const FeaturedBooksFailure({required this.errMessage});
+}
